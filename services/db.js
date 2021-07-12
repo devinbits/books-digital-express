c2d919e5d16cc42535ae755214f455b8dd5f4fbe
Abhinav Chandra <adityanabhinav@gmail.com>
2021-07-12 19:06:03 +0530

    update db service

--- services/db.js
+++ services/db.js
@@ -1,9 +1,10 @@
 const mysql = require("mysql2/promise");
 const config = require("../utils/config");
 
+const connection = mysql.createPool(config.db);
+
 async function query(sql, params) {
   console.log(sql, params);
-  const connection = mysql.createPool(config.db);
   const [results] = await connection.execute(sql, params);
   return results;
 }
